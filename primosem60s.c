134b2d216be390249c91bbcdc3a25d25fecda349
Gabriela Tenório <gabitenoriob@gmail.com>
2022-11-04 15:00:08 -0300

    Update primosem60s.c

--- primosem60s.c
+++ primosem60s.c
@@ -1,30 +1,24 @@
-#include <stdio.h>
-#include <time.h>
 #include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
 
-int primos(int n,int contador)
-{
-    if(n == contador)
-    {
-        printf("%d\n", n);
-    }
-    else if(n % contador == 0 || n < 2)
-    {
-        primos(n+1,1);
-    }
-    else
-    {
-        primos(n,contador + 1);// se for menor q 2 ou 2 desconsidera e retorna fazendo dnv
-    }
+int primos(int n, int contador) {
+  if (n == contador) {
+    printf("%d\n", n);
+    primos(n + 1, contador + 1);
+  } else if (n % contador == 0 || n < 2) {
+    primos(n + 1, 2);
+  } else {
+    primos(n,
+           contador +
+               1); // se for menor q 2 ou 2 desconsidera e retorna fazendo dnv
+  }
 }
-void saida(int sig)
-{
-    exit(0);
-}
-int main()
-{
-    signal(SIGALARM, saida);
-    alarm(60);
-    primos(0,1);
+void saida(int sig) { exit(0); }
+int main() {
+  signal(SIGALRM, saida);
+  alarm(60);
+  primos(2, 2);
 }
